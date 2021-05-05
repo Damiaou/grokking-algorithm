1babb9e972fa54bf0b6757121e0021c592cf7ec7
Dam <damsauvagere@gmail.com>
2021-05-05 15:48:32 +0200

    Initial commit

---
+++ src/binarySearch.js
@@ -0,0 +1,7 @@
+export function binarySearch(list, item) {
+  let low = 0;
+  let high = list.length;
+  while (low !== high) {
+    // Do stuff
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello there!</h1>
+<div>
+  This repo is mainly about algorithm, learning and playing with them thanks to <a target='_blank' href=\'https://www.manning.com/books/grokking-algorithms\'>this book</a>
+
+</div>
+`;
